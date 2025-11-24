6f7aab7b5a5ef667ea067b034519b7b40d09ec89
Omar Abdulla <OmarAbdulla7@hotmail.com>
2025-11-24 18:09:02 +0300

    Update the copy_from_calldata_removes_bytes_data tests

--- fixtures/solidity/translated_semantic_tests/calldata/copy_from_calldata_removes_bytes_data/copy_from_calldata_removes_bytes_data.sol
+++ fixtures/solidity/translated_semantic_tests/calldata/copy_from_calldata_removes_bytes_data/copy_from_calldata_removes_bytes_data.sol
@@ -3,15 +3,19 @@ contract c {
         data = msg.data;
         return true;
     }
+
     function checkIfDataIsEmpty() public view returns (bool) {
         return data.length == 0;
     }
+
     function sendMessage() public returns (bool, bytes memory) {
         bytes memory emptyData;
-        return address(this).call{gas: 100_000}(emptyData);
+        return address(this).call{gas: 10_000_000_000}(emptyData);
     }
+
     fallback() external {
         data = msg.data;
     }
+
     bytes data;
 }
